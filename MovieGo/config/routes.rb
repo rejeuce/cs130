9a9782cb901b5ee29cbd215bf8c9289e7c733040
rejeuce <rejeuce07@gmail.com>
2016-10-31 18:05:22 -0700

    Created empty login, home, map. Also moved Sam's html into map.

--- MovieGo/config/routes.rb
+++ MovieGo/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'login/login'
+
+  get 'home/home'
+
   get 'map/map'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
