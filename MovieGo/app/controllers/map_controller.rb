0105de15953003104a3ad87dfa80525c84c9ed78
Sam Rubenacker <srubenacker@ucla.edu>
2016-11-15 12:59:42 -0800

    Added passing the users location to the map screen.  Just zip code for now.  Added location and movie info variables to Map controller to be accessed in map view.

--- MovieGo/app/controllers/map_controller.rb
+++ MovieGo/app/controllers/map_controller.rb
@@ -6,14 +6,16 @@ class MapController < ApplicationController
     require 'uri'
     require 'json'
 
-    puts params[:movie_info]
-    decoded_uri = URI.unescape(params[:movie_info])
-    puts decoded_uri
+    #puts params[:movie_info]
+    @decoded_uri = URI.unescape(params[:movie_info])
+    #puts decoded_uri
     parsed_json = JSON.parse(decoded_uri)
-    puts parsed_json
+    #puts parsed_json
 
     @title = parsed_json['title']
-    puts @title
+    #puts @title
+
+    @location = params[:location]
 
     render "map"
   end
