a3fe9fad6c4db7f278cbc059fe64ee657613a29c
Sam Rubenacker <srubenacker@ucla.edu>
2016-11-15 13:19:01 -0800

    Attempt to fix bug with ruby .inspect in JS.

--- MovieGo/app/controllers/map_controller.rb
+++ MovieGo/app/controllers/map_controller.rb
@@ -9,10 +9,10 @@ class MapController < ApplicationController
     #puts params[:movie_info]
     @decoded_uri = URI.unescape(params[:movie_info])
     #puts decoded_uri
-    parsed_json = JSON.parse(@decoded_uri)
+    @parsed_json = JSON.parse(@decoded_uri)
     #puts parsed_json
 
-    @title = parsed_json['title']
+    @title = @parsed_json['title']
     #puts @title
 
     @location = params[:location]
