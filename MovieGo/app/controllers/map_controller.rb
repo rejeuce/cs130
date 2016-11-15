8f333a314bbe713e123f4d2befc24b435e3b4634
Sam Rubenacker <srubenacker@ucla.edu>
2016-11-15 13:03:50 -0800

    Fixed undefined local variable bug.

--- MovieGo/app/controllers/map_controller.rb
+++ MovieGo/app/controllers/map_controller.rb
@@ -9,7 +9,7 @@ class MapController < ApplicationController
     #puts params[:movie_info]
     @decoded_uri = URI.unescape(params[:movie_info])
     #puts decoded_uri
-    parsed_json = JSON.parse(decoded_uri)
+    parsed_json = JSON.parse(@decoded_uri)
     #puts parsed_json
 
     @title = parsed_json['title']
