3e17cae1f3d4f8c064050d96353e2be242f1924c
Sam Rubenacker <srubenacker@ucla.edu>
2016-11-14 19:41:33 -0800

    Rendered the map view from the create function of the map controller.

--- MovieGo/app/controllers/map_controller.rb
+++ MovieGo/app/controllers/map_controller.rb
@@ -3,7 +3,19 @@ class MapController < ApplicationController
   end
 
   def create
-    @title = params[:title]
+    require 'uri'
+    require 'json'
+
+    puts params[:movie_info]
+    decoded_uri = URI.unescape(params[:movie_info])
+    puts decoded_uri
+    parsed_json = JSON.parse(decoded_uri)
+    puts parsed_json
+
+    @title = parsed_json['title']
+    puts @title
+
+    render "map"
   end
 
 
