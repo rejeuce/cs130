c3d49246f432ea9e2517905cd3c7f7d2d668e3f5
gitlev <michaelb.levin@gmail.com>
2016-11-26 00:15:12 -0800

    ml/jp - added rescue

--- MovieGo/app/controllers/application_controller.rb
+++ MovieGo/app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+
     rescue ActiveRecord::RecordNotFound
   end
   helper_method :current_user
