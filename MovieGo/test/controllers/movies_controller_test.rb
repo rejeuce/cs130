cd8ac3bac5b188e1da67e99efcd7071ecfae7d9e
rejeuce <rejeuce07@gmail.com>
2016-11-28 00:13:12 -0800

    Added simple HTML tests for map and movie controllers

--- MovieGo/test/controllers/movies_controller_test.rb
+++ MovieGo/test/controllers/movies_controller_test.rb
@@ -7,4 +7,23 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  def setup_webstub(host_url = 'http://www.example.com/')
+    @url = host_url
+
+    stub_request(:head, @url)
+        .with(headers: { 'Accept': '*/*', 'User-Agent': 'Ruby' })
+        .to_return(status: 200,
+                   body: '',
+                   headers: { 'content-type': 'image/jpg' })
+  end
+
+  test 'populated html successfully' do
+    setup_webstub
+    get "/movies"
+    assert_select "p", "Movies Near You:"
+    assert_select "input", 1
+    assert_select "button","Search"
+    assert_select "button", 3
+  end
+
 end
