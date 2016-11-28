66eabcff7a9c8851a13916e9976994bd3f2a16ca
jalapagos <jalaj.pun@gmail.com>
2016-11-27 16:09:17 -0800

    Made final changes, login is working. Need to use Michael's API key so we can all see the project

--- MovieGo/config/routes.rb
+++ MovieGo/config/routes.rb
@@ -1,4 +1,5 @@
-Rails.application.routes.draw do
+=begin
+<Rails.application.routes.draw do
 
   root 'home#landing'
 
@@ -26,4 +27,20 @@ MovieGo::Application.routes.draw do
     resource :home, only: [:show]
 
     root to: "home#show"
+end
+=end
+MovieGo::Application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+    get 'auth/failure', to: redirect('/')
+    get 'signout', to: 'sessions#destroy', as: 'signout'
+ 
+    resources :sessions, only: [:create, :destroy]
+    resource :home, only: [:show]
+    root to: "home#landing"
+    get 'login' => 'login#login'
+  get 'home' => 'home#home'
+    get 'home/landing' => 'home#landing'
+  get 'map' => 'map#map'
+  post 'map' => 'map#create'
+  get 'movies' => 'movies#list'
 end
