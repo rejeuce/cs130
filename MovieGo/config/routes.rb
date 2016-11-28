2bc8ea85a29295d84d4e8a56ab107c69ebd42e33
jalapagos <jalaj.pun@gmail.com>
2016-11-27 16:10:56 -0800

    Made few changes, aligned shit

--- MovieGo/config/routes.rb
+++ MovieGo/config/routes.rb
@@ -1,34 +1,3 @@
-=begin
-<Rails.application.routes.draw do
-
-  root 'home#landing'
-
-  get 'login' => 'login#login'
-
-  get 'home' => 'home#home'
-
-  get 'home/landing' => 'home#landing'
-
-  get 'map' => 'map#map'
-
-  post 'map' => 'map#create'
-
-  get 'movies' => 'movies#list'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-
-MovieGo::Application.routes.draw do
-    get 'auth/:provider/callback', to: 'sessions#create'
-    get 'auth/failure', to: redirect('/')
-    get 'signout', to: 'sessions#destroy', as: 'signout'
-
-    resources :sessions, only: [:create, :destroy]
-    resource :home, only: [:show]
-
-    root to: "home#show"
-end
-=end
 MovieGo::Application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
     get 'auth/failure', to: redirect('/')
